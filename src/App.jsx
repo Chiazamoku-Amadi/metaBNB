59be54c0e46efc720f9bf7d9e695ba43121b8195
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-11-13 03:17:30 +0100

    refactor: Imported the header component, built the hero section of the website.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import heroHouse1 from "./assets/heroHouse1.webp";
+import heroHouse2 from "./assets/heroHouse2.webp";
+import heroHouse3 from "./assets/heroHouse3.webp";
+import heroHouse4 from "./assets/heroHouse4.webp";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <section className="container">
+        <Header />
+        <div className="hero">
+          <div className="hero-text">
+            <h1 className="hero-text-title">
+              Rent a <span className="purple">Place</span> away from{" "}
+              <span className="purple">Home</span> in the{" "}
+              <span className="purple">Metaverse</span>
+            </h1>
+            <p className="hero-text-description">
+              we provide you access to luxury and affordable houses in the
+              metaverse, get a chance to turn your imagination to reality at
+              your comfort zone
+            </p>
+            <div className="hero-search">
+              <input
+                type="text"
+                placeholder="Search for location"
+                className="search-input"
+              />
+              <label htmlFor="search">
+                <button className="search-btn">Search</button>
+              </label>
+            </div>
+          </div>
+          <div className="hero-images">
+            <div className="hero-images1">
+              <img src={heroHouse1} className="hero-image" />
+              <img src={heroHouse2} className="hero-image" />
+            </div>
+            <div className="hero-images2">
+              <img src={heroHouse3} className="hero-image" />
+              <img src={heroHouse4} className="hero-image" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
