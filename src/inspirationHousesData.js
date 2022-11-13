0e04be56e9568ba4ae146bfae6ae8ac4833e2832
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-11-13 06:27:36 +0100

    refactor: Created this jsx file to hold data used in the inspiration section of the website.

---
+++ src/inspirationHousesData.js
@@ -0,0 +1,87 @@
+import house1 from "./assets/house1.webp";
+import house2 from "./assets/house2.webp";
+import house3 from "./assets/house3.webp";
+import house4 from "./assets/house4.webp";
+import house5 from "./assets/house5.webp";
+import house6 from "./assets/house6.webp";
+import house7 from "./assets/house7.webp";
+import house8 from "./assets/house8.webp";
+import rating from "./assets/rating.webp";
+import { nanoid } from "nanoid";
+
+const inspirationHousesData = [
+  {
+    id: nanoid(),
+    picture: house1,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house2,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house3,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house4,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house5,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house6,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house7,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house8,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+];
+
+export default inspirationHousesData;
