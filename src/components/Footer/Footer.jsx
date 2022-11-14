d0ce45fad323efd48004e45fcf61e41db0f3ce70
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-11-14 20:49:33 +0100

    refactor: Changed the code structure of the footer to make it responsive, installed react-icon as a dependency. style: Made the website responsive on mobile, tablet, and laptop views.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,37 @@ import CopyrightIcon from "../../assets/copyrightIcon.webp";
 function Footer() {
   return (
     <section className="footer-container">
-      <div className="logo-section">
-        <img src={FooterLogo} />
+      <div className="logo-quicklinks">
+        <img src={FooterLogo} className="footer-logo" />
+        <nav className="community">
+          <p>Community</p>
+          <ul>
+            <li>NFT</li>
+            <li>Tokens</li>
+            <li>Landlords</li>
+            <li>Discord</li>
+          </ul>
+        </nav>
+        <nav className="places">
+          <p>Places</p>
+          <ul>
+            <li>Castle</li>
+            <li>Farms</li>
+            <li>Beach</li>
+            <li>Learn more</li>
+          </ul>
+        </nav>
+        <nav className="about-us">
+          <p>About Us</p>
+          <ul>
+            <li>Road map</li>
+            <li>Creators</li>
+            <li>Career</li>
+            <li>Contact us</li>
+          </ul>
+        </nav>
+      </div>
+      <div className="social-copyright-section">
         <div className="socials">
           <img src={FacebookIcon} />
           <img src={InstagramIcon} />
@@ -21,33 +50,6 @@ function Footer() {
           <p>{new Date().getFullYear()} Metabnb</p>
         </div>
       </div>
-      <nav className="community">
-        <p>Community</p>
-        <ul>
-          <li>NFT</li>
-          <li>Tokens</li>
-          <li>Landlords</li>
-          <li>Discord</li>
-        </ul>
-      </nav>
-      <nav className="places">
-        <p>Places</p>
-        <ul>
-          <li>Castle</li>
-          <li>Farms</li>
-          <li>Beach</li>
-          <li>Learn more</li>
-        </ul>
-      </nav>
-      <nav className="about-us">
-        <p>About Us</p>
-        <ul>
-          <li>Road map</li>
-          <li>Creators</li>
-          <li>Career</li>
-          <li>Contact us</li>
-        </ul>
-      </nav>
     </section>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.webp";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 
 function Header() {
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  function handleNavbarClick() {
+    setOpenNavbar(!openNavbar);
+  }
+
   return (
     <div className="header">
-      <img src={Logo} className="header-logo" />
-      <nav>
-        <ul className="navbar">
-          <li>Home</li>
-          <li>Place to Stay</li>
-          <li>NFTs</li>
-          <li>Community</li>
-        </ul>
-      </nav>
-      <button className="header-btn">Connect Wallet</button>
+      <div className="header-logo-container">
+        <img src={Logo} className="header-logo" />
+      </div>
+      <div className="menu">
+        <nav>
+          <button onClick={handleNavbarClick} className="hamburger">
+            {openNavbar ? <GrClose /> : <GiHamburgerMenu />}
+          </button>
+          <ul className={openNavbar ? "hamburgerNavbar" : "navbar"}>
+            <li>Home</li>
+            <li>Place to Stay</li>
+            <li>NFTs</li>
+            <li>Community</li>
+          </ul>
+        </nav>
+        <button className="header-btn">Connect Wallet</button>
+      </div>
     </div>
   );
 }
