5d044fc8f98b9f0087b2ba21c01153ca79beb276
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-11-16 05:37:02 +0100

    refactor: Imported 16 pictures and created an array of objects for the card data used in PlaceToStay.jsx

---
+++ src/placeToStayHousesData.js
@@ -0,0 +1,167 @@
+import house1 from "./assets/house1.webp";
+import house2 from "./assets/house2.webp";
+import house3 from "./assets/house3.webp";
+import house4 from "./assets/house4.webp";
+import house5 from "./assets/house5.webp";
+import house6 from "./assets/house6.webp";
+import house7 from "./assets/house7.webp";
+import house8 from "./assets/house8.webp";
+import house9 from "./assets/house9.webp";
+import house10 from "./assets/house10.webp";
+import house11 from "./assets/house11.webp";
+import house12 from "./assets/house12.webp";
+import house13 from "./assets/house13.webp";
+import house14 from "./assets/house14.webp";
+import house15 from "./assets/house15.webp";
+import house16 from "./assets/house16.webp";
+import rating from "./assets/rating.webp";
+import { nanoid } from "nanoid";
+
+const placeToStayHousesData = [
+  {
+    id: nanoid(),
+    picture: house5,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house6,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house7,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house8,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house1,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house2,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house3,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house4,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house9,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house10,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house11,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house12,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house13,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house14,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house15,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+  {
+    id: nanoid(),
+    picture: house16,
+    title: "Desert King",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2 weeks stay",
+    rating: rating,
+  },
+];
+
+export default placeToStayHousesData;
