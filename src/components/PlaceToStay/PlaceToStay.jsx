e167816ecdffb1b314d8bf36157c280450b3f00e
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-11-16 05:38:40 +0100

    refactor: Built the dynamic parts of the place to stay page. style: Styled the dynamic parts of the place to stay page.

--- src/components/PlaceToStay/PlaceToStay.jsx
+++ src/components/PlaceToStay/PlaceToStay.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import placeToStayHousesData from "../../placeToStayHousesData";
+import Cards from "../Cards/Cards";
 import "./PlaceToStay.css";
+import Settings from "../../assets/settings.webp";
 
 function PlaceToStay() {
+  const placeToStayHouseCards = placeToStayHousesData.map(
+    (placeToStayHouseCard) => {
+      return <Cards key={placeToStayHouseCard.id} {...placeToStayHouseCard} />;
+    }
+  );
+
   return (
-    <div>
-      <h1>PlaceToStay</h1>
-    </div>
+    <section className="place-to-stay-container">
+      <div className="filters">
+        <p>Restaurant</p>
+        <p>Cottage</p>
+        <p>Castle</p>
+        <p>fantast city</p>
+        <p>beach</p>
+        <p>Carbins</p>
+        <p>Off grid</p>
+        <p>Farm</p>
+        <button className="location">
+          Location <img src={Settings} className="settings" />
+        </button>
+      </div>
+      <div className="place-to-stay-house-cards">{placeToStayHouseCards}</div>
+    </section>
   );
 }
 
